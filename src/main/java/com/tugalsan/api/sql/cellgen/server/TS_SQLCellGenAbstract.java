a93fdb3531b8b71925ef87425f8bb127e1191d90
tugalsan karabacak <tugalsan@users.noreply.github.com>
2025-03-24 09:15:03 +0300

    .

--- src/main/java/com/tugalsan/api/sql/cellgen/server/TS_SQLCellGenAbstract.java
+++ src/main/java/com/tugalsan/api/sql/cellgen/server/TS_SQLCellGenAbstract.java
@@ -1,10 +1,10 @@
 package com.tugalsan.api.sql.cellgen.server;
 
 
-import com.tugalsan.api.function.client.maythrow.uncheckedexceptions.TGS_FuncMTUCE_In1;
+import com.tugalsan.api.function.client.maythrowexceptions.unchecked.TGS_FuncMTU_In1;
 import com.tugalsan.api.tuple.client.*;
 
-abstract public class TS_SQLCellGenAbstract<E, V> implements TGS_FuncMTUCE_In1<TGS_Tuple1<V>> {
+abstract public class TS_SQLCellGenAbstract<E, V> implements TGS_FuncMTU_In1<TGS_Tuple1<V>> {
 
     public TS_SQLCellGenAbstract(E executor, int colIdx) {
         this.executor = executor;
